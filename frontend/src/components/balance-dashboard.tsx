57c859fc3cd71dd45002685dac49dec6a8d3b822
Mina Demian <mina@minademian.com>
2022-06-02 16:40:50 +0200

    refactor: prop slide

--- frontend/src/components/balance-dashboard.tsx
+++ frontend/src/components/balance-dashboard.tsx
@@ -1,5 +1,8 @@
-import React from "react";
-import { IAccountInfoDetail, IData, IUserDetail } from "../lib/interfaces";
+import {
+  IAccountInfoDetail,
+  IUserDetail,
+  TTransactionsList,
+} from "../lib/interfaces";
 import { AccountBalance } from "./account-balance";
 import { SalaryWidget } from "./salary-widget";
 import { TransfersBalance } from "./transfers-balance";
@@ -7,17 +10,22 @@ import { TransfersBalance } from "./transfers-balance";
 export function BalanceDashboard(props: {
   userInfo: IUserDetail;
   accountInfo: IAccountInfoDetail;
+  transactions: TTransactionsList;
 }) {
   return (
     <section>
       <section>
-        <SalaryWidget />
+        <SalaryWidget salary={props.accountInfo.salary} />
       </section>
       <section>
-        <AccountBalance />
+        <AccountBalance balance={props.accountInfo.balance} />
       </section>
       <section>
-        <TransfersBalance />
+        <TransfersBalance
+          balance={props.transactions
+            .map((transaction) => transaction.amount)
+            .reduce((previous, current) => previous + current, 0)}
+        />
       </section>
     </section>
   );
