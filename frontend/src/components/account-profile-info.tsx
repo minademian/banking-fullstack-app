11ea1a24f914f86c175464b169d51b062fe41986
Mina Demian <mina@minademian.com>
2022-06-02 20:31:07 +0200

    feat: basic static impl of component

--- frontend/src/components/account-profile-info.tsx
+++ frontend/src/components/account-profile-info.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import { useState } from "react";
+import { IAccountInfoDetail, IUserDetail } from "../lib/interfaces";
 
-export function AccountProfileInfo() {
+export function AccountProfileInfo(props: {
+  userInfo: IUserDetail;
+  account: IAccountInfoDetail;
+}) {
+  const [user, updateUser] = useState(props.userInfo);
+  const [account, updateAccount] = useState(props.account);
+
+  const anonymizeAccountNumber = (accountNr: string) => {
+    return Array.from(accountNr).splice(-4, 4);
+  };
+
   return (
-    <div>
-      <img></img>
-      <caption></caption>
-    </div>
+    <header>
+      <img src={process.env.PUBLIC_URL + user.img}></img>
+      <caption>
+        <h4>{user.name}</h4>
+        <h6>Account ending with {anonymizeAccountNumber(account.nr)}</h6>
+      </caption>
+    </header>
   );
 }
