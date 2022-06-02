3dd1b469960cbea2d47d71427e31b19e6fb44327
Mina Demian <mina@minademian.com>
2022-06-02 16:40:36 +0200

    refactor: prop slide

--- frontend/src/components/account-dashboard.tsx
+++ frontend/src/components/account-dashboard.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BalanceDashboard } from "./balance-dashboard";
 import { TransactionsList } from "./transactions-list";
 import { IData } from "../lib/interfaces";
+import { AccountProfileInfo } from "./account-profile-info";
 
 const CURRENCY = {
   code: "USD",
@@ -14,8 +15,13 @@ export function AccountDashboard(props: { data: IData }) {
   return (
     <main>
       <p>Account Dashboard</p>
+      <AccountProfileInfo />
       <section>
-        <BalanceDashboard userInfo={data.user} accountInfo={data.account} />
+        <BalanceDashboard
+          userInfo={data.user}
+          accountInfo={data.account}
+          transactions={data.transactions}
+        />
       </section>
       <section>
         <TransactionsList list={data.transactions} />
